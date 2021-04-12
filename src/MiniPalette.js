085e80e682f45d5205b1704edc2d050840c04ed3
IMPrimph <vishnugajulapalli123@gmail.com>
2021-04-12 20:51:48 +0530

    start styling miniPalette

---
+++ src/MiniPalette.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles'
+
+const styles = {
+    root: {
+        backgroundColor: "white",
+        border: '1px solid black',
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: 'relative',
+        overflow: 'hidden',
+        "&:hover": {
+            cursor: "pointer"
+        }
+    },
+    colors: {
+        backgroundColor: 'grey'
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
+    },
+    emoji: {
+        marginLeft: '0.5rem',
+        fontSize: "1.5rem"
+    }
+}
+
+function Minipalette(props) {
+    const { classes, paletteName, emoji } = props;
+    return (
+        <div className={classes.root}>
+            <div className={classes.colors}></div>
+            <div className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></div>
+        </div>
+    )
+}
+
+export default withStyles(styles)(Minipalette)
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import Minipalette from './MiniPalette';
+import { withStyles } from '@material-ui/styles'
 
 class PaletteList extends Component {
     render() {
         const { palettes } = this.props;
         return (
             <div>
+                <Minipalette />
                 <h1>React Colors</h1>
                 {palettes.map(palette => (
-                    <p>
-                        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-                    </p>
-
+                    // <p>
+                    //     <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+                    // </p>
+                    <Minipalette {...palette} />
                 ))}
             </div>
         );
