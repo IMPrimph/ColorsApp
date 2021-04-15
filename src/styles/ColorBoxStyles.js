68e08b7a21dfdda5e4a1fe1e39d7d354f5502799
IMPrimph <vishnugajulapalli123@gmail.com>
2021-04-15 20:35:18 +0530

    make colorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,15 +15,15 @@ const styles = {
         },
         [sizes.down('lg')]: {
             width: '25%',
-            height: props => (props.showingFullPalette ? '20%' : '50%'),
+            height: props => (props.showingFullPalette ? '20%' : '33.33333%'),
         },
         [sizes.down('md')]: {
             width: '50%',
-            height: props => (props.showingFullPalette ? '10%' : '50%'),
+            height: props => (props.showingFullPalette ? '9.9%' : '20%'),
         },
         [sizes.down('xs')]: {
             width: '100%',
-            height: props => (props.showingFullPalette ? '10%' : '10%'),
+            height: props => (props.showingFullPalette ? '4.9%' : '10%'),
         }
     },
     copyText: {
@@ -112,7 +112,10 @@ const styles = {
             textAlign: "center",
             marginBottom: "0",
             padding: "1rem",
-            textTransform: "uppercase"
+            textTransform: "uppercase",
+            [sizes.down("xs")]: {
+                fontSize: "6rem"
+            }
         },
         "& p": {
             fontSize: "2rem",
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes'
+
 const styles = {
     Palette: {
         height: "100vh",
@@ -39,7 +41,20 @@ const styles = {
             textTransform: "uppercase",
             border: "none",
             textDecoration: "none"
+        },
+        [sizes.down('lg')]: {
+            width: '25%',
+            height: '33.33333%'
+        },
+        [sizes.down('md')]: {
+            width: '50%',
+            height: '20%'
+        },
+        [sizes.down('xs')]: {
+            width: '100%',
+            height: '10%'
         }
+
     }
 }
 
