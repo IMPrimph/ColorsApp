ee6d3c905593ac544553a626d0a1dda6f3ee457a
IMPrimph <vishnugajulapalli123@gmail.com>
2021-04-15 18:23:57 +0530

    save palettes to localStorage

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
 
   constructor(props) {
     super(props)
+    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'))
     this.state = {
-      palettes: seedColors
+      palettes: savedPalettes || seedColors
     }
     this.savePalette = this.savePalette.bind(this)
     this.findPalette = this.findPalette.bind(this)
   }
-
 
 
   findPalette(id) {
@@ -27,7 +27,12 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] })
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage)
+  }
+
+  syncLocalStorage() {
+    //save palettes to local storage
+    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
   }
 
   render() {
