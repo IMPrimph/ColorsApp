8440b14915e8842510628e9173c95db6ceae17d2
IMPrimph <vishnugajulapalli123@gmail.com>
2021-04-15 18:45:23 +0530

    add deleteIcon to miniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles'
 import styles from './styles/MiniPaletteStyles'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function Minipalette(props) {
     const { classes, paletteName, emoji, colors, handleClick } = props;
@@ -13,8 +14,14 @@ function Minipalette(props) {
     ));
     return (
         <div className={classes.root} onClick={handleClick}>
+            <div className={classes.delete}>
+                <DeleteIcon className={classes.deleteIcon} style={{ transition: 'all 0.3s ease-in-out' }} />
+            </div>
             <div className={classes.colors}>{miniColorBoxes}</div>
-            <div className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></div>
+            <h5
+                className={classes.title}>
+                {paletteName} <span className={classes.emoji}>{emoji}</span>
+            </h5>
         </div>
     )
 }
--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -6,9 +6,11 @@ const styles = {
         padding: "0.5rem",
         position: 'relative',
         overflow: 'hidden',
-        "&:hover": {
-            cursor: "pointer"
-        }
+        cursor: "pointer",
+        "&:hover svg": {
+            opacity: 1
+        },
+
     },
     colors: {
         backgroundColor: '#dae1e4',
@@ -40,6 +42,22 @@ const styles = {
         margin: "0 auto",
         position: "relative",
         marginBottom: "-3.5px"
+    },
+    deleteIcon: {
+        color: "white",
+        backgroundColor: '#eb3d30',
+        width: "20px",
+        height: '20px',
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        padding: '10px',
+        zIndex: 10,
+        opacity: 0,
+        transition: 'all 0.3s ease-in-out'
+    },
+    delete: {
+
     }
 }
 
