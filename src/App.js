9511272e2df587b8f8157bef1942257e62d6163f
IMPrimph <vishnugajulapalli123@gmail.com>
2021-04-16 12:11:58 +0530

    add 404 route

--- src/App.js
+++ src/App.js
@@ -109,7 +109,17 @@ class App extends Component {
                 )}
               />
 
-
+              <Route
+                render={(routeProps) => (
+                  <Page>
+                    <PaletteList
+                      palettes={this.state.palettes}
+                      deletePalette={this.deletePalette}
+                      {...routeProps}
+                    />
+                  </Page>
+                )}
+              />
 
             </Switch>
           </CSSTransition>
